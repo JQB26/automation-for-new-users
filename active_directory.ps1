c196f5cec290e32e194c40383d656e9f4c627ec8
JQB26 <kubaszczepanek@interia.pl>
2021-08-26 09:40:28 +0200

    command line controller (new_user.py)

--- active_directory.ps1
+++ active_directory.ps1
@@ -11,3 +11,15 @@ $dispName = $name + " " + $surname + " (" + $group + ")"
 $group += $patdata
 
 New-ADUser -Name $dispName -Surname $surname -SamAccountName $alias -DisplayName $dispName -AccountPassword(Read-Host -AsSecureString "Input Password") -Enabled $true
+
+
+
+
+New-ADUser -SamAccountName $user.sammaccountname -Name $user.name `
+ -DisplayName $user.name -GivenName $user.givenname -Surname $user.surname`
+  -UserPrincipalName $user.userprincipialname -Path $user.distinguished -Company $user.Company`
+  -EmailAddress $user.emailaddress -Office $user.department -Title $user.title`
+  -AccountPassword (ConvertTo-SecureString -AsPlainText $user.password -Force) -Enabled $true -ChangePasswordAtLogon $true
+
+ Add-ADGroupMember biuro -Members $user.sammaccountname
+Add-ADGroupMember zarzady -Members $user.sammaccountname
